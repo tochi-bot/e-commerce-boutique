c7c5f9d502ce00240e1f65a743e86651eb8da620
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-09-07 12:28:26 +0000

    Added products details functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, Product_id):
+    """ A view to show individual product details """
+
+    products = get_objects_or_404(product, pk=Product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
