bedf7a44120b2dd2e0bbab46c3621325cfc75652
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-10-13 06:25:40 +0000

    DEPLOYMENT

--- boutique/settings.py
+++ boutique/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Define the hosts your application can serve. This prevents HTTP Host header attacks.
 ALLOWED_HOSTS = [
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
      # Other
     'crispy_forms',
     'profiles',
+    'storages',
 ]
 
 # Authentication backends. Order matters: Django tries them in the given order.
@@ -175,8 +176,26 @@ STATIC_URL = 'static/'  # URL prefix for static files
 STATICFILES_DIRS=  (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'bourtique-ado'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 # Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # Default primary key field type
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # Default primary key field type
 
 
 # Stripe
